e60ef3c3d08226911b03d7301e7d5fc529068c75
unknown <al16014@sic.shibaura-it.ac.jp>
2018-05-29 14:07:33 +0900

    initial commit

---
+++ KyomuServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace KyomuServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var listner = new HttpListener();
+            
+        }
+    }
+}
