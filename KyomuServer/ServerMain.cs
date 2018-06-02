3abd674caf9f95093a7e147547cc566a59682831
Unknown <tanuki13.snow@gmail.com>
2018-06-02 15:56:53 +0900

    json.net&npgsql
    ここからブランチを切ってほしい

---
+++ KyomuServer/ServerMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+
+namespace KyomuServer
+{
+    class ServerMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var listner = new HttpListener();
+            var json = new JObject();//これがjson
+            var jvar = new JValue("hoge"); //jsonの数値
+            var jary = new JArray();//jsonの配列
+        }
+    }
+}
