2627a757d96a96d5aaad573e5c0209b30d213a9a
Unknown <tanuki13.snow@gmail.com>
2018-06-02 16:30:35 +0900

    made method
    sfusen と saccountの関数の外枠

--- KyomuServer/sAccount.cs
+++ KyomuServer/sAccount.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace KyomuServer
 {
-    class sAccount
+    class sAccount  //アカウント情報を扱う
     {
+        static JObject AccountCreate(string accountID)
+        {
+
+        }
+
+        static JObject AccountRefer(string accountID)
+        {
+            
+        }
+        
     }
 }
---
+++ KyomuServer/sFusen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace KyomuServer
+{
+    class sFusen //付箋情報を扱う
+    {
+        static JObject GetFusenData(string accountID)
+        {
+
+        }
+
+
+        static int CreateFusen(int accountID, out int statusCode)
+        {
+
+        }
+        static void UpdateFusen(JObject fusenData, out int statusCode)
+        {
+
+        }
+        static void DeleteFusen(int accountID,int fusenID, out int statusCode)
+        {
+
+        }
+
+    }
+}
