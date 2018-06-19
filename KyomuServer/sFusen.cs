971bb0de6839796844e6ddc3d4669261d6233952
T-T <al16065@shibaura-it.ac.jp>
2018-06-19 15:38:20 +0900

    sFusen
    GetAllFusen

--- KyomuServer/sFusen.cs
+++ KyomuServer/sFusen.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Linq;
+using Npgsql;
 
 namespace KyomuServer
 {
@@ -9,24 +8,57 @@ namespace KyomuServer
     {
         public static string Connection { set { Connection = value; } get { return Connection; } }
 
-        public static JObject GetFusenData(string accountID)
+        public static JArray GetFusenAllData(int accountID, out int statusCode)
         {
+            JArray UserFusen = new JArray();
+            using (var conn = new Npgsql.NpgsqlConnection(Connection))
+            {
+                conn.Open();
+                var dataReader = new NpgsqlCommand($@"select * from fusen where accountID = {accountID}", conn).ExecuteReader();
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        JObject jobj = new JObject();
+                        int userID = (int)dataReader["accountID"];
+                        jobj.Add("accountID", new JValue(userID));
+                        int fusenID = (int)dataReader["fusenID"];
+                        jobj.Add("fusenID", new JValue(fusenID));
+                        string title = (string)dataReader["title"];
+                        jobj.Add("titleID", new JValue(title));
+                        string[] tag = (string[])dataReader["tag"];
+                        jobj.Add("tag", new JValue(tag));
+                        string text = (string)dataReader["text"];
+                        jobj.Add("text", new JValue(text));
+                        string color = (string)dataReader["color"];
+                        jobj.Add("color", new JValue(color));
+                        UserFusen.Add(jobj);
+                    }
+
+                }
+                conn.Close();
+            }
+            statusCode = 200;
+            return UserFusen;
+        }
+
+        public static JObject CreateFusen(int accountID, int fusenID, out int statusCode)
+        {
+            //DBに接続してアカウントID,付箋IDを持つ行を追加
+            //成功したらそのまま返す/失敗したらJObjectに入れて返す
+            var db = new DbContext();
+            statusCode = 0;
             return null;
         }
-
-
-        public static int CreateFusen(int accountID, out int statusCode)
+        public static JObject UpdateFusen(int accountID, int fusenID, JObject fusenData, out int statusCode)
         {
             statusCode = 0;
-            return 0;
+            return null;
         }
-        public static void UpdateFusen(JObject fusenData, out int statusCode)
+        public static JObject DeleteFusen(int accountID, int fusenID, out int statusCode)
         {
             statusCode = 0;
-        }
-        public static void DeleteFusen(int accountID,int fusenID, out int statusCode)
-        {
-            statusCode = 0;
+            return null;
         }
 
     }
